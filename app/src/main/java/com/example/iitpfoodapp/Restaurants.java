aefb4234c241763582b57dc6d0e14fbbbb7a2efd
vaibhavqwerty <vaibhav131099@gmail.com>
2019-02-08 19:01:40 +0530

    Further Changes

--- app/src/main/java/com/example/iitpfoodapp/Restaurants.java
+++ app/src/main/java/com/example/iitpfoodapp/Restaurants.java
@@ -1,7 +1,10 @@
 package com.example.iitpfoodapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Restaurants extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class Restaurants extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurants);
+        setTitle("Restaurants");
+        TextView arya=findViewById(R.id.arya);
+        arya.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(Restaurants.this,AryaCanteen.class);
+                startActivity(i);
+            }
+        });
     }
 }
