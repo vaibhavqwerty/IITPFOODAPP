34d1c09e54c31146b11b7110e22ab3766f351cea
vaibhavqwerty <vaibhav131099@gmail.com>
2019-02-21 10:51:02 +0530

    Sign in is added

--- app/src/main/java/com/example/iitpfoodapp/MainActivity.java
+++ app/src/main/java/com/example/iitpfoodapp/MainActivity.java
@@ -1,34 +1,88 @@
 package com.example.iitpfoodapp;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
    //Checking for Commit on the github
+//private String mUsername;
+
+       List<AuthUI.IdpConfig> providers = Arrays.asList(
+               new AuthUI.IdpConfig.EmailBuilder().build(),
+               new AuthUI.IdpConfig.GoogleBuilder().build());
+   public static final int RC_SIGN_IN=1;
+private FirebaseAuth mFirebaseAuth;
+private FirebaseAuth.AuthStateListener mAuthStateListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("Login Account");
 
-        TextView signUp=findViewById(R.id.signup);
-        signUp.setOnClickListener(new View.OnClickListener() {
+//Authentication
+        mFirebaseAuth=FirebaseAuth.getInstance();
+        Button con =findViewById(R.id.con);
+        con.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent signUp =new Intent(MainActivity.this, signUp.class);
-                startActivity(signUp);
+                Intent i =new Intent(MainActivity.this,Restaurants.class);
+                startActivity(i);
             }
         });
-        TextView signIn=findViewById(R.id.LOGIN);
-        signIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent signIn =new Intent(MainActivity.this, Restaurants.class);
-                startActivity(signIn);
-            }
-        });
+
+
+mAuthStateListener= new FirebaseAuth.AuthStateListener() {
+    @Override
+    public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+        FirebaseUser user=firebaseAuth.getCurrentUser();
+        if(user!=null)
+        {
+          //user is signed in
+            Toast.makeText(MainActivity.this,"You are now signed in. Welcome to FreindlyChat!", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            //user is signed out
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setIsSmartLockEnabled(false)
+                            .setAvailableProviders(providers)
+                            .setTheme(R.style.AppTheme)
+                            .build(),
+                    RC_SIGN_IN);
+
+        }
+    }
+};
+
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
     }
 }
